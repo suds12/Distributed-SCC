eef5d4912b33f4c6bfbe70d509e0ae4fe13e6480
Boyana Norris <brnorris03@gmail.com>
2020-07-24 20:25:46 -0700

    enable multiple partitionings with one graph reading

--- README.md
+++ README.md
@@ -28,3 +28,27 @@ For example
 ./partition input/inputgraph 2
 ```
 This will create two partitioning files, each containing lines that are pairs of partition ID and vertex number.
+
+Note that both ASCII and binary edgelist  formats are supported (each edge is represented with three ints -- source vertex ID, target vertex ID, weight).
+Multiple partitionings can be generated at once by specifying a list instead of a single number of partitions, e.g.,
+```
+./partition input/inputgraph.edgelist.bin 2,3
+```
+This will produce the files
+```
+inputgraph.edgelist.part_002_0.bin
+inputgraph.edgelist.part_002_1.bin
+inputgraph.edgelist.part_003_0.bin
+inputgraph.edgelist.part_003_1.bin
+inputgraph.edgelist.part_003_2.bin
+``
+where `part_002_1` indicates partition 1 of a 2-part graph partitioning and `part_003_2` indicates partition 2 of a 3-part graph partitioning.
+
+## 2. Utilities
+
+The convert programer takes an ASCII edgelist graph as input and produces the binary equivalent.
+Build with `make convert`. To run:
+```
+./convert inputgraphfile
+```
+will produce inputgraphfile.edgelist.bin binary edgelist file.
--- partition.cpp
+++ partition.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <iomanip>
 
 #include "graphReader.hpp"
 
@@ -14,93 +15,102 @@
 // Adjust makefile, then build with "make partition"
 // To run:  ./partition
 
+using namespace std;
+
 int main(int argc, char *argv[]){
 
     if (argc < 3) {
-        cerr << "Usage: ./partition inputgraph num_part\n\tinputgraph: edgelist graph input\n\tnum_part: number of partitions\n";
+        cerr << "Usage: ./partition inputgraph num_part\n\tinputgraph: edgelist graph input\n\tnum_part: number of partitions, e.g., 4 or a comma-separated list of values, e.g., 2,4,8,16\n";
         exit(1);
     }
     string filename = argv[1];
-    idx_t nParts    = stoi(argv[2]);
- 
-    cout << "Partitioning " << filename << " into " << nParts << " partitions...\n";
+    string partstr(argv[2]);
+    vector<int> nparts;
+    stringstream ss(partstr);
+    string token;
+    while (std::getline(ss, token, ','))  
+       nparts.push_back(stoi(token)); 
 
-    idx_t nWeights  = 1; // only one weight value per vertex
-
-
-    GraphReader reader(1000);   // buffer size is 1000
     cout << "Reading graph...\n";
+    idx_t nWeights  = 1; // only one weight value per vertex
+    GraphReader reader(1000);   // buffer size is 1000
     reader.read(filename);      
 
-    idx_t nVert = reader.get_num_vert();
-    auto xadj = reader.get_adj_ind();
-    auto adjncy = reader.get_adj_vert();
-    idx_t objval;
-    std::vector<idx_t> part(nVert, 0);
-    cout << "Partitioning graph with " << nVert << " vertices...\n";
-    int ret = METIS_PartGraphKway(&nVert,& nWeights, xadj.data(), adjncy.data(),
-				  NULL, NULL, NULL, &nParts, NULL,
-       				  NULL, NULL, &objval, part.data());
+    for (auto it = nparts.begin(); it != nparts.end(); it++) {
+        int nParts = *it;
+ 
+        cout << "Partitioning " << filename << " into " << nParts << " partitions...\n";
+
+
+        idx_t nVert = reader.get_num_vert();
+        auto xadj = reader.get_adj_ind();
+        auto adjncy = reader.get_adj_vert();
+        idx_t objval;
+        std::vector<idx_t> part(nVert, 0);
+        cout << "Partitioning graph with " << nVert << " vertices...\n";
+        int ret = METIS_PartGraphKway(&nVert,& nWeights, xadj.data(), adjncy.data(),
+				    NULL, NULL, NULL, &nParts, NULL,
+       				    NULL, NULL, &objval, part.data());
 
 #if DEBUG >= 2
-    std::cout << ret << std::endl;
-    for (unsigned part_i = 0; part_i < part.size(); part_i++){
-	std::cout << part_i << " " << part[part_i] << std::endl;
-    }
+        std::cout << ret << std::endl;
+        for (unsigned part_i = 0; part_i < part.size(); part_i++){
+	    std::cout << part_i << " " << part[part_i] << std::endl;
+        }
 #endif
 
-    // Create mapping: a vector corresponding to the partitions, where each entry is the list of vertices 
-    // in that partition
-    vector<vector<idx_t>> partition(nParts);
-    for (unsigned vertex = 0; vertex < part.size(); vertex++){
-        partition[part[vertex]].push_back(vertex);
-    }
+        // Create mapping: a vector corresponding to the partitions, where each entry is the list of vertices 
+        // in that partition
+        vector<vector<idx_t>> partition(nParts);
+        for (unsigned vertex = 0; vertex < part.size(); vertex++){
+            partition[part[vertex]].push_back(vertex);
+        }
 #if DEBUG>=1
-    for (unsigned i = 0; i < nParts; i++) {
-        cout << i << ": ";
-        for (unsigned j = 0; j < partition[i].size(); j++) 
-            cout << partition[i][j] << " ";
-        cout << endl;
-    }
+        for (unsigned i = 0; i < nParts; i++) {
+            cout << i << ": ";
+            for (unsigned j = 0; j < partition[i].size(); j++) 
+                cout << partition[i][j] << " ";
+            cout << endl;
+        }
 #endif
 
-    // Write out partitioning files
-
-    
-    for(unsigned i = 0; i < nParts; i++) {
-        stringstream partition_filename_stream;
-        partition_filename_stream << filename <<  "_" << i;
-        string pfilename = partition_filename_stream.str();
-        ofstream partfile(pfilename, ios::out | ios::binary);
-        idx_t *vertices = partition[i].data();
-        partfile.write(reinterpret_cast<char *>(vertices), partition[i].size() * sizeof(idx_t));
-        // For ASCII output, change to: 
-        //ofstream partfile(partition_filename.str());
-        //for (unsigned j = 0; j < partition[i].size(); j++) partfile << partition[i][j] << endl;
-        partfile.close();
-    }
+        // Write out partitioning files
+        for(unsigned i = 0; i < nParts; i++) {
+            string filename_base = filename.substr(0,filename.rfind('.'));
+            ostringstream partition_filename_stream;
+            partition_filename_stream << filename_base <<  ".part_" << std::setw(3) << std::setfill('0') << nParts << "_" << i << ".bin";
+            string pfilename = partition_filename_stream.str();
+            ofstream partfile(pfilename, ios::out | ios::binary);
+            idx_t *vertices = partition[i].data();
+            partfile.write(reinterpret_cast<char *>(vertices), partition[i].size() * sizeof(idx_t));
+            // For ASCII output, change to: 
+            //ofstream partfile(partition_filename.str());
+            //for (unsigned j = 0; j < partition[i].size(); j++) partfile << partition[i][j] << endl;
+            partfile.close();
+        }
 
 #if DEBUG>=1
-    // Read the partititioning data and create the same partitioning data structure
-    vector<vector<idx_t>> inp_partition(nParts);
-    cout << "After reading binary:\n"; 
-    for(unsigned i = 0; i < nParts; i++) {
-        stringstream partition_filename_stream;
-        partition_filename_stream << filename <<  "_" << i;
-        string pfilename = partition_filename_stream.str();
-       
-        ifstream fin(pfilename, ios::in | ios::binary);
-        fin.seekg(0, ifstream::end);
-        int size = fin.tellg() / sizeof (idx_t);
-        idx_t tmp[size];
-        fin.seekg(0, ifstream::beg);
-        fin.read(reinterpret_cast<char *>(&tmp), sizeof(tmp));
-        cout << i <<  ": ";
-        for (int j=0; j < size; j++) cout << tmp[j] << " ";
-        cout << endl;
-        std::move(tmp,tmp+size,back_inserter(inp_partition[i]));
-        fin.close();
-    }
+        // Read the partititioning data and create the same partitioning data structure
+        vector<vector<idx_t>> inp_partition(nParts);
+        cout << "After reading binary:\n"; 
+        for(unsigned i = 0; i < nParts; i++) {
+            stringstream partition_filename_stream;
+            partition_filename_stream << filename <<  "_" << i;
+            string pfilename = partition_filename_stream.str();
+        
+            ifstream fin(pfilename, ios::in | ios::binary);
+            fin.seekg(0, ifstream::end);
+            int size = fin.tellg() / sizeof (idx_t);
+            idx_t tmp[size];
+            fin.seekg(0, ifstream::beg);
+            fin.read(reinterpret_cast<char *>(&tmp), sizeof(tmp));
+            cout << i <<  ": ";
+            for (int j=0; j < size; j++) cout << tmp[j] << " ";
+            cout << endl;
+            std::move(tmp,tmp+size,back_inserter(inp_partition[i]));
+            fin.close();
+        }
 #endif
+    }   // number of partitions loop
     return 0;
 }
